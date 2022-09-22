8c8c905c645e1bd3c7230c4e17f0c80665e29c47
jgsbd210945 <jagordon9522@gmail.com>
2022-09-22 16:53:25 -0400

    RORT 5.2 and 5.3

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal "Ruby on Rails Tutorial", full_title
+    assert_equal "Help | Ruby on Rails Tutorial", full_title("Help")
+  end
 end
