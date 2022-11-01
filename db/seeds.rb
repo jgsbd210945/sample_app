aad4e9c37e6618632a19046414e9a38928315598
jgsbd210945 <jagordon9522@gmail.com>
2022-11-01 08:41:39 -0400

    RORT 13.1-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 User.create!(name:  "Example User",
     email: "example@railstutorial.org",
     password:              "foobar",
-    password_confirmation: "foobar"
+    password_confirmation: "foobar",
     admin: true)
 
 # Generate a bunch of additional users.
@@ -14,4 +14,11 @@ User.create!(name:  name,
       email: email,
       password:              password,
       password_confirmation: password)
+end
+
+# Generate microposts for a subset of users.
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
